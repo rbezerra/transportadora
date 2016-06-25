893ea46615e882e3399624094160acbe71cb4d30
Ramon Bezerra <ramonbezerra90@gmail.com>
2016-06-25 14:08:09 -0300

    finalizando front-end

--- routes/fretes.js
+++ routes/fretes.js
@@ -1,9 +1,16 @@
+import logger from "../libs/logger.js";
+
 module.exports = app => {
 	const Fretes = app.db.models.Fretes;
+	const Cidades = app.db.models.Cidades;
+	const Clientes = app.db.models.Clientes;
 
 	app.route("/fretes")
 		.get((req, res) => {
-			Fretes.findAll({})
+			Fretes
+				.findAll({
+					include: [{model: Cidades},{model: Clientes}]
+				})
 				.then(result => res.json(result))
 				.catch(error => {
 					res.status(412).json({msg: error.message});
@@ -13,13 +20,17 @@ module.exports = app => {
 			Fretes.create(req.body)
 				.then(result => res.json(result))
 				.catch(error => {
+					logger.error(`[${new Date()}] ${error.message}`);
 					res.status(412).json({msg: error.message});
 				});
 		});
 
 	app.route("/fretes/:codigo_frete")
 		.get((req, res) => {
-			Fretes.findOne({where: req.params})
+			Fretes.findOne({
+					where: req.params,
+					include: [{model: Cidades}, {model: Clientes}]
+				})
 				.then(result => {
 					if(result){
 						res.json(result);
@@ -29,12 +40,14 @@ module.exports = app => {
 				})
 				.catch(error => {
 					res.status(412).json({msg: error.message});
+					logger.error(`[${new Date()}] ${error.message}`);
 				});
 		})
 		.put((req, res) => {
 			Fretes.update(req.body, {where: req.params})
 				.then(result => res.sendStatus(204))
 				.catch(error => {
+					logger.error(`[${new Date()}] ${error.message}`);
 					res.status(412).json({msg: error.message});
 				});
 		})
@@ -42,6 +55,7 @@ module.exports = app => {
 			Fretes.destroy({where: req.params})
 				.then(result => res.sendStatus(204))
 				.catch(error => {
+					logger.error(`[${new Date()}] ${error.message}`);
 					res.status(412).json({msg: error.message});
 				});
 		});
